7e45450e14c2daca2bb609008a38a8ff976addaf
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-02-05 18:03:54 -0500

    added allowed requestors and listeners

--- clickshare-chrome-ext/background.js
+++ clickshare-chrome-ext/background.js
@@ -4,3 +4,13 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+//ADDED LISTENERS HERE- Michael
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log({ request })
+});
+
+chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
+  console.log("Received message from " + sender + ": ", request);
+  sendResponse({ received: true }); //respond however you like
+});
