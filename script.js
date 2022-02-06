b76e68045e28cad421e98cd1a0a679c968da561a
Luca Borletti <lgborletti@gmail.com>
2022-02-05 21:46:28 -0500

    created 'enter' linking for input field of chat

--- script.js
+++ script.js
@@ -82,6 +82,20 @@ const trackClick = (e) => {
   );
 };
 
+
+var input = document.getElementById("message-input");
+
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("send-message-btn").click();
+  }
+});
+
+
 $("#generate-conference-id").click(() => {
   // Generate a random conference alias
   let conferenceAlias = Math.round(Math.random() * 10000);
