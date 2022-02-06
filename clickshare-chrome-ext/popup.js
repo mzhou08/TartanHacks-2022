22d67245309aeb8ceb5c7194b04554b2a1dc301c
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-02-06 01:39:54 -0500

    MADE FIREBASE CONNECTION WORK!!!!!!!

--- clickshare-chrome-ext/popup.js
+++ clickshare-chrome-ext/popup.js
@@ -1,29 +1,30 @@
 // Initialize button with user's preferred color
 let confIdSubmit = document.getElementById("confIdSubmit");
+let confIdForm = document.getElementById("confIdForm");
 
 // chrome.storage.sync.get("color", ({ color }) => {
 //   changeColor.style.backgroundColor = color;
 // });
 
 // When the submit button is clicked, use setClickerPos to move the clicker
+// confIdForm.submit(function (e) {e.preventDefault();});
 
 confIdSubmit.addEventListener("click", async () => {
-    let [tab] = chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     //   chrome.tabs.sendMessage(tabs[0].id, {createDiv: {width: "100px", height: "100px", innerHTML: "Hello"}}, function(response) {
     //     console.log(response.confirmation);
     //   });
     // });
 
-    console.log("hello?");
     let conferenceID = document.getElementById("conference-id-input").value;
 
     console.log(conferenceID);
   
-    // chrome.scripting.executeScript({
-    //   target: { tabId: tab.id },
-    //   function: setClickerPos,
-    // });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setClickerPos,
+    });
 });
 
 
@@ -31,12 +32,12 @@ confIdSubmit.addEventListener("click", async () => {
   // The body of this function will be executed as a content script inside the
   // current page
 function setClickerPos() {
-    fetch("https://tartan-hacks-2022-default-rtdb.firebaseio.com/coords.json")
+    fetch("https://tartan-hacks-2022-default-rtdb.firebaseio.com/4912.json")
     .then((response) => {
-      return response.json;
+      return response.json();
     })
     .then((data) => {
-      const clickCoords = [];
+      let clickCoords = [];
 
       for (const key in data) {
         const coords = {
@@ -47,6 +48,8 @@ function setClickerPos() {
         clickCoords.unshift(coords);
       }
 
-      console.log(coords);
+      console.log(clickCoords);
+      document.title = clickCoords[0];
     })
+
 }
